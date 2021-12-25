decd34c7c53584d432f269f4015a18160860d80f
Sivan Kaplan <sivanagar@gmail.com>
2021-12-24 18:41:16 -0800

    cliet side

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -25,6 +26,16 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
